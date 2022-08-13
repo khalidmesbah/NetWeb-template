f0ae169c29c1e84d16411fa3dffbb539a575a97d
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-08-14 00:55:06 +0200

    up

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // make the mega-menu active on hover
-
 const showMegaMenuEl = document.getElementById("show-mega-menu");
-const MegaMenuEl = document.querySelector(".mega-menu");
+const megaMenuEl = document.querySelector(".mega-menu");
 
-showMegaMenuEl.addEventListener("mouseover", () => {
-  MegaMenuEl.classList.add("mega-menu-active"); 
-  MegaMenuEl.focus();
-});
-MegaMenuEl.addEventListener("focusout", () =>
-  MegaMenuEl.classList.remove("mega-menu-active")
+showMegaMenuEl.addEventListener(
+  "mouseover", () => megaMenuEl.classList.add("mega-menu-active")
 );
+showMegaMenuEl.addEventListener(
+  "mouseleave", () => megaMenuEl.classList.remove("mega-menu-active")
+);
+
+/* skills */
+function isScrolledIntoView(elem) {
+  var rect = elem.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+
+  return isVisible;
+}
+const skills = document.getElementById(`skills`);
+
+window.addEventListener(`scroll`, (e) => {
+  if (isScrolledIntoView(skills)) {
+    const fills = document.querySelectorAll(`.fill`);
+    fills.forEach(fill =>{
+      fill.style.width = fill.dataset.width
+    });
+  }
+});
